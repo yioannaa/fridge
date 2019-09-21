64a58ea87c09167905d80cc62460dad6a38cafea
railsgirls <joanna.krzyzanowska@o2.pl>
2019-09-21 14:29:17 +0200

    shopping list added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,8 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products_fridge = Product.where(bought: true)
+@products_todo = Product.where(bought: false)
   end
 
   # GET /products/new
